b0714a3ad032c269db88a63195e827d6d02857d9
alexandru.banut <alexandru.banut@emag.ro>
2022-09-14 16:10:20 +0300

    Shareposts app

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+//DB Params
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'YOUR_USER');
+define('DB_PASS', 'YOUR_PASS');
+define('DB_NAME', 'YOUR_DB_NAME');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost:8080/shareposts');
+
+//Site Name
+define('SITENAME', 'SharePosts');
+
+//App Version
+define('APPVERSION', '1.0.0');
+
